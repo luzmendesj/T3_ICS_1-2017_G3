f6c7cc11de2129e351f5ae340fe3c704c8cfccd2
luzmendesj <jorge.jglm@gmail.com>
2017-06-08 00:31:56 -0300

    Melodia Gerada.

--- Trabalho3.java
+++ Trabalho3.java
@@ -1,5 +1,5 @@
 /*
-Trabalho 3 de Introdu√ß√£o √† Computa√ß√£o S√¥nica
+Trabalho 3 de Introducao a Computacao Sonica
 
 Jorge Mendes        12/0014599
 Marcos Fleury       12/00
@@ -9,6 +9,7 @@ Paulo Santos        12/005
 
 //Packages de Som
 import sintese.*;
+import sintese.Melodia;
 import javax.sound.midi.*;
 
 //Packages de I/O
@@ -26,13 +27,14 @@ import static java.lang.Math.pow;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class Trabalho3 implements Runnable
 {
     public Sequencer sequenciador = null;
     public Sequence sequencia = null;
     List<String> Informacoes = new ArrayList<>();
-    
+    public Melodia melodiaPronta;
     public static void main(String[] args){
 
         Trabalho3 var = new Trabalho3();
@@ -142,6 +144,11 @@ public class Trabalho3 implements Runnable
     public List<String> getMidi() {
         
         //Pega as trilhas do Midi lido
+        double duracao = sequencia.getMicrosecondLength()/1000.0d;
+        double totalTiques = sequencia.getTickLength();
+
+        double durTique = duracao/totalTiques;
+        
         Track[] trilhas = sequencia.getTracks();
         Informacoes = new ArrayList<String>();
         
@@ -183,15 +190,16 @@ public class Trabalho3 implements Runnable
                 switch(byte0)
                 {
                     case 0b10000000:    
-                        nomecomando = msgBytes[0] + "noteOFF"; 
+                        nomecomando = "noteOFF"; 
                         f = 55f*pow(2, msgBytes[1]/12);
-                        noteOFF_v.get(canal).add(new NoteOFF(f, tique)); 
+                        infoAdicional = "       Frequencia: " + f ; 
+                        noteOFF_v.get(canal).add(new NoteOFF(f, tique*durTique)); 
                         break;
                     case 0b10010000:
                         nomecomando = "noteON";
                         f = 55f*pow(2, msgBytes[1]/12);
                         infoAdicional = "       Frequencia: " + f ; 
-                        noteON_v.get(canal).add(new NoteON(f, msgBytes[2], tique));    
+                        noteON_v.get(canal).add(new NoteON(f, msgBytes[2], tique*durTique));    
                         break; 
                     case 0b10110000:    nomecomando = "Control Change"; break;
                     case 0b11000000:    nomecomando = "Program Change"; break;  
@@ -201,7 +209,7 @@ public class Trabalho3 implements Runnable
                     //---(introduzir outros casos)
                 }
 
-                Informacoes.add("       Mensagem: " + nomecomando );
+                Informacoes.add("       Mensagem: " + nomecomando + "   No canal " + canal);
                 Informacoes.add(infoAdicional);
                 Informacoes.add("       Instante: " + tique );
                 Informacoes.add("  ------------------------------------------");
@@ -210,8 +218,10 @@ public class Trabalho3 implements Runnable
             Informacoes.add("  Fim da trilha n\u00ba " + i + " **********************");
             Informacoes.add("  ------------------------------------------");
         }
-        Informacoes.add(noteON_v.toString());
-        Informacoes.add(noteOFF_v.toString());
+        //Informacoes.add(noteON_v.toString());
+        //Informacoes.add(noteOFF_v.toString());
+        
+        melodiaPronta =geraMelodia(noteON_v, noteOFF_v);
         return Informacoes;
     }
     
@@ -220,30 +230,127 @@ public class Trabalho3 implements Runnable
             } catch(InterruptedException e){ }
 
     }
+    
+    Melodia geraMelodia(ArrayList<ArrayList<NoteON>> noteON_v, ArrayList<ArrayList<NoteOFF>> noteOFF_v){
+        
+        int n_canais = noteON_v.size();
+        Melodia M = new Melodia();
+        
+        //Para cada canal
+        for (int i = 0; i < n_canais; i++){
+            if (noteON_v.get(i).isEmpty()) continue;
+            //Adiciona o silÍncio ao comeÁo da trilha
+            double inicioCanal = noteON_v.get(i).get(0).getInicio();
+            if (noteON_v.get(i).get(0).getInicio() != 0){
+                Nota primeiraNota = new Nota((float) inicioCanal);
+                M.addNota(primeiraNota);
+            }
+                
+            //Para cada NoteON
+            for(int j = 0; j < noteON_v.get(i).size(); j++){
+                //Encontra o par de note On e note off
+                NoteON n_ON = noteON_v.get(i).get(j);
+                //int prox_off = noteOFF_v.get(i).indexOf(new NoteOFF(n_ON.getFreq(), n_ON.getInicio()));
+                int prox_off = 0;
+                for (int k = 0; k < noteOFF_v.get(i).size(); k++){
+                    if (n_ON.getFreq() ==  noteOFF_v.get(i).get(k).getFreq()){
+                        prox_off = k;
+                        break;
+                    }
+                }
+                //System.out.println(prox_off);
+                NoteOFF n_OFF = noteOFF_v.get(i).get(prox_off);
+                
+                double duracaoNota = n_ON.getInicio() - n_OFF.getFim();
+                noteOFF_v.get(i).remove(prox_off);
+                Nota novaNota = new Nota(duracaoNota, n_ON.getFreq(), n_ON.getIntensidade());
+                
+                //Insere nota correspondente
+                M.addNota(novaNota);
+                
+                ///Insere silÍncio atÈ a prÛxima nota
+                
+                
+            }
+        }
+        return M;
+    }
 }
+
+
 
 class NoteON{
     public double freq;
-    public int intensidade;
-    public long inicio;
+    public double intensidade;
+    public double inicio;
     
-    public NoteON(double f, int i, long s){
+    public NoteON(double f, double i, double s){
         this.freq = f;
         this.intensidade = i;
         this.inicio = s;
     }
     
+    double getFreq(){
+        return this.freq;
+    }
+    
+    double getIntensidade(){
+        return this.intensidade;
+    }
+    
+    double getInicio(){
+        return this.inicio;
+    }
             
 }
 
-class NoteOFF{
+class NoteOFF implements Comparable<NoteOFF>{
     public double freq;
-    public long fim;
+    public double fim;
     
-    public NoteOFF(double f, long s){
+    public NoteOFF(double f, double s){
         this.freq = f;
         
         this.fim = s;
     }
     
+    double getFreq(){
+        return this.freq;
+    }
+    
+    double getFim(){
+        return this.fim;
+    }
+    
+    public boolean equals(NoteOFF nOff){
+        if (!(nOff instanceof NoteOFF))
+            return false;
+        if (nOff == null || this == null)
+            return false;
+            
+            
+        if(this.freq == nOff.getFreq()) 
+            return true;
+        else 
+            return false;
+        
+    }
+    
+    @Override
+    public int compareTo(NoteOFF nOff){
+        if (this.freq < nOff.getFreq()){
+            return -1;
+        }
+        else if (this.freq == nOff.getFreq()){
+            if (this.fim < nOff.getFim()){
+                return -1;
+            }
+            else if (this.fim == nOff.getFim()){
+                return 0;
+            }
+            else return 1;
+        }
+        else return 1;
+            
+    }
 }
